ca2fe3dd594b8777abbc3b0ab7f1d60db8451b17
OTHMANE345 <ballateothmane32@gmail.com>
2025-12-13 01:25:24 +0100

    creation of custom task item

---
+++ frontend/src/app/components/custom-item/custom-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomItemComponent } from './custom-item.component';
+
+describe('CustomItemComponent', () => {
+  let component: CustomItemComponent;
+  let fixture: ComponentFixture<CustomItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomItemComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CustomItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/custom-item/custom-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-item.component.html',
+  styleUrl: './custom-item.component.css'
+})
+export class CustomItemComponent {
+
+}
