63756017272d5e764cd34e6275065a7f801d06df
OTHMANE345 <ballateothmane32@gmail.com>
2025-12-13 01:24:57 +0100

    Creation of custom input

---
+++ frontend/src/app/components/custom-input/custom-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './custom-input.component.html',
+  styleUrl: './custom-input.component.css',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CustomInputComponent),
+    multi: true
+  }]
+})
+export class CustomInputComponent {
+ @Input() label : string = '';
+//  @Input() placeholder : string = '';
+  @Input() type : string = '';
+  @Input() placeholder : string = '';
+  @Input() control : FormControl = new FormControl();
+}
